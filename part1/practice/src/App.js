cfca5c6da36e827b60ed150ef602402e121ca940
ooepi <robert.harmaakivi@gmail.com>
2022-06-18 12:12:13 +0300

    Finished practice

---
+++ part1/practice/src/App.js
@@ -0,0 +1,33 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/ooepi">ooepi</a>
+    </div>
+  )
+
+}
+
+
+const App = () => {  
+  const nimi = 'Pekka'
+  const ika = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika}/>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
