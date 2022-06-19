8f0263e684ef12944113b3d9527ba66fbb6fcf4a
ooepi <robert.harmaakivi@gmail.com>
2022-06-19 16:59:24 +0300

    Practice for part 1.c

--- part1/practice/src/App.js
+++ part1/practice/src/App.js
@@ -1,33 +1,69 @@
-const Hello = (props) => {
+import { useState } from 'react'
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({handleClick, text}) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+  
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreceByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <Display counter={counter}/>
+      <Button 
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button 
+        handleClick={decreceByOne}
+        text='minus'
+      />
+      <Button 
+        handleClick={setToZero}
+        text='zero'
+      />
     </div>
   )
 }
 
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/ooepi">ooepi</a>
-    </div>
-  )
-
-}
 
 
-const App = () => {  
-  const nimi = 'Pekka'
-  const ika = 10
 
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika}/>
-      <Footer />
-    </div>
-  )
-}
+// const Hello = ({name, age}) => {
+
+//   const bornYear = () => new Date().getFullYear() - age
+
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>So you were probably born {bornYear()}</p>
+//     </div>
+//   )
+// }
+
+
+// const App = () => {
+//   const nimi = 'Pekka'
+//   const ika = 10
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10} />
+//       <Hello name={nimi} age={ika} />
+//     </div>
+//   )
+// }
 
 export default App
