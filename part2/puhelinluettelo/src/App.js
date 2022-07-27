90abf8fbe2557f94d8ab898f2ae31d1df7b57520
ooepi <robert.harmaakivi@gmail.com>
2022-07-27 15:09:19 +0300

    Puhelinluettelo step 3 done

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -5,45 +5,58 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       name: 'Arto Hellas',
+      number: '+358441234567',
       id: 1
     }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const AddName = (event) => {
+  const AddPerson = (event) => {
     event.preventDefault()
-
-    // console.log(persons)
-    // console.log(newName)
 
     if (persons.some(person => person.name === newName)){
       alert(`${newName} is already added to phonebook`)
       setNewName('')
+      setNewNumber('')
       return
     }
 
     const personObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1
     }
 
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={AddName}>
+      <form onSubmit={AddPerson}>
         <div>
           name: 
           <input 
             value={newName}
             onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number: 
+          <input 
+            value={newNumber}
+            onChange={handleNumberChange}
           />
         </div>
         <div>
