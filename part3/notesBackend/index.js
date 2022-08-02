d9a3ead80c0791e29168f33288dfc877ce680e3f
ooepi <robert.harmaakivi@gmail.com>
2022-08-02 11:53:28 +0300

    part 3b notes done

--- part3/notesBackend/index.js
+++ part3/notesBackend/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 
 let notes = [
     {
@@ -80,7 +83,7 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
